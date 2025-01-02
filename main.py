d25c1a5504a5434afefb1c82a15c08cf0126fc44
edsonmendieta <edsonj.mendieta@gmail.com>
2025-01-02 02:54:05 -0800

    added character frequency function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def read_book(filepath: str) -> str:
 
-    with open("books/frankenstein.txt") as file_object:
+    with open(filepath) as file_object:
         book_text = file_object.read()
         
         return book_text
@@ -12,12 +12,24 @@ def word_count(text: str) -> int:
 
     return len(words_list)     
 
+def char_frequency(text: str) -> dict:
 
+    frequency_dict = {}
 
+    lowercase_string = text.lower()
+    no_white_space_string = lowercase_string.replace(" ", "")
+
+    for char in no_white_space_string:
+        if char not in frequency_dict:
+            frequency_dict[char] = 1
+        else:
+            frequency_dict[char] += 1
+    
+    return frequency_dict
 
 
 def main():
 
-    print(word_count(read_book("books/frankenstein.txt")))
+    print(char_frequency(read_book("books/frankenstein.txt")))
 
 main()
